7eb9bf3da7503140becbc599a2a2d8b90d60aac3
Mateusz Gąsior <mr.nimelo@gmail.com>
2015-05-17 23:13:44 +0200

    Commit 3 49%

--- CatsVsDogs/ConsoleApplication/Program.cs
+++ CatsVsDogs/ConsoleApplication/Program.cs
@@ -25,14 +25,119 @@ namespace ConsoleApplication
         static void Main(string[] args)
         {
             int SECTIONS = 10;
-            //1:40 dla 1000
-            //0:14 dla 100
-            var cats = Perform(@"E:\Uczelnia\sem6\BIAI\train\cats", 10);
-            var dogs = Perform(@"E:\Uczelnia\sem6\BIAI\train\dogs", 10);
+            int SHREDEDELEMENTS = 0;
 
-            var catHistograms = cats.HueHistogram(SECTIONS);
-            var dogHistograms = dogs.HueHistogram(SECTIONS);
+            //var cats = Perform(@"E:\Uczelnia\sem6\BIAI\train\cats", 50, 0, 50, 5, out SHREDEDELEMENTS);
+            //var dogs = Perform(@"E:\Uczelnia\sem6\BIAI\train\dogs", 50, 0, 50, 5, out SHREDEDELEMENTS);
 
+            //var catHistograms = cats.HueHistogram(SECTIONS);
+            //var dogHistograms = dogs.HueHistogram(SECTIONS);
+
+            //// ConsoleOutput(SECTIONS, catHistograms, dogHistograms);
+
+
+            List<int[]> sourceInput = new List<int[]>();
+            List<int> sourceOut = new List<int>();
+
+            //AddToSource(ref sourceInput, ref sourceOut, catHistograms, 0, SHREDEDELEMENTS);
+            //AddToSource(ref sourceInput, ref sourceOut, dogHistograms, 1, SHREDEDELEMENTS);
+            Console.WriteLine("LoadingData");
+            for(int i = 0; i < 500; i+=50)
+            {
+                
+                AddToSource(ref sourceInput, ref sourceOut, Perform(@"E:\Uczelnia\sem6\BIAI\train\cats", 50, i, 50, 5, out SHREDEDELEMENTS).HueHistogram(SECTIONS), 0, SHREDEDELEMENTS);
+                AddToSource(ref sourceInput, ref sourceOut, Perform(@"E:\Uczelnia\sem6\BIAI\train\dogs", 50, i, 50, 5, out SHREDEDELEMENTS).HueHistogram(SECTIONS), 1, SHREDEDELEMENTS);
+                if(i % 1000 == 0)
+                {
+                    Console.WriteLine(i + 1000);
+                    Console.WriteLine(DateTime.Now - System.Diagnostics.Process.GetCurrentProcess().StartTime);
+                }
+            }
+           
+
+
+            int[][] inputs;
+            int[] outputs;
+
+            DecisionTree tree = GenerateDecisionTree(SHREDEDELEMENTS, SECTIONS, 2);
+            ID3Learning id3learning = new ID3Learning(tree);
+           // MergeInputOutput(sourceInput, sourceOut, SHREDEDELEMENTS, out inputs, out outputs);
+            //AddToSource(catHistograms, dogHistograms, SHREDEDELEMENTS, out inputs, out outputs);
+
+            // Learn the training instances!
+            id3learning.Run(sourceInput.ToArray(), sourceOut.ToArray());
+           // Console.WriteLine(id3learning.ComputeError(inputs, outputs));
+
+            int findedCats = 0;
+            int findedDogs = 0;
+
+            Console.WriteLine("Query");
+            int index = 0;
+            for(int iteration = 0; iteration < 12500; iteration += 50)
+            {
+                if(iteration % 1000 == 0)
+                {
+                    Console.WriteLine(iteration);
+                    Console.WriteLine(DateTime.Now - System.Diagnostics.Process.GetCurrentProcess().StartTime);
+                }
+                var test = Perform(@"E:\Uczelnia\sem6\BIAI\test1\test1", 50, iteration, 50, 5, out SHREDEDELEMENTS);
+                var testHistograms = test.HueHistogram(SECTIONS);
+                foreach(var item in testHistograms)
+                {
+                    int[] query = item.ToArray();
+                    int output = tree.Compute(query);
+
+                    if(output == 0)
+                    {
+                        findedCats++;
+                    }
+                    else
+                    {
+                        findedDogs++;
+                    }
+
+                    File.AppendAllText("dogscats.txt", index + 1 + "," + output + Environment.NewLine);
+                    index++;
+                }
+
+            }
+            Console.WriteLine("Dogs: {0}\nCats: {1}", findedDogs, findedCats);
+            Console.WriteLine(DateTime.Now - System.Diagnostics.Process.GetCurrentProcess().StartTime);
+
+            // File.Create(@"dogscats.txt");
+
+        }
+
+        private static void AddToSource(ref List<int[]> source, ref List<int> sourceOut, IEnumerable<IEnumerable<int>> collection, int param, int shreddedElements)
+        {
+            foreach(var item in collection)
+            {
+                var tmp = new int[shreddedElements];
+                Array.Copy(item.ToArray(), tmp, shreddedElements);
+
+                source.Add(tmp);
+                sourceOut.Add(param);
+            }
+        }
+
+        private static void MergeInputOutput(List<int[]> source, List<int> sourceOut, int shreddedElements, out int[][] inputs, out int[] outputs)
+        {
+            int yk = 0;
+            inputs = new int[source.Count()][];
+            outputs = new int[source.Count()];
+
+            foreach(var item in source)
+            {
+                inputs[yk] = new int[shreddedElements];
+                Array.Copy(item.ToArray(), inputs[yk], shreddedElements);
+                outputs[yk] = sourceOut.ElementAt(yk);
+                yk++;
+            }
+        }
+
+
+        private static void ConsoleOutput(int SECTIONS, IEnumerable<IEnumerable<int>> catHistograms, IEnumerable<IEnumerable<int>> dogHistograms)
+        {
             foreach(var item in catHistograms.Histogram(SECTIONS))
             {
                 item.ToList().ForEach(x => Console.Write(x + " "));
@@ -48,89 +153,37 @@ namespace ConsoleApplication
             }
 
             Console.WriteLine(DateTime.Now - System.Diagnostics.Process.GetCurrentProcess().StartTime);
-
-
-
-
-            DataTable table = new DataTable("Example");
-
-            for(int i = 0; i < 10; i++)
-            {
-                table.Columns.Add(i.ToString());
-            }
-
-            table.Rows.Add(catHistograms);
-
-           // Codification codebook = new Codification(table);
-
-            //DataTable symbols = codebook.Apply(table);
-
-            DecisionVariable[] attrubites = 
-            {
-                  new DecisionVariable("0", new AForge.IntRange(0, 9)),
-                 new DecisionVariable("1", new AForge.IntRange(0, 9)),
-                  new DecisionVariable("2", new AForge.IntRange(0, 9)),
-                   new DecisionVariable("3", new AForge.IntRange(0, 9)),
-                  new DecisionVariable("4", new AForge.IntRange(0, 9)),
-                   new DecisionVariable("5", new AForge.IntRange(0, 9)),
-                  new DecisionVariable("6", new AForge.IntRange(0, 9)),
-                   new DecisionVariable("7", new AForge.IntRange(0, 9)),
-                  new DecisionVariable("8", new AForge.IntRange(0, 9)),
-                   new DecisionVariable("9", new AForge.IntRange(0, 9))
-                
-            };
-
-            int classCOunt = 2;
-
-            DecisionTree tree = new DecisionTree(attrubites, classCOunt);
-
-            // Create a new instance of the ID3 algorithm
-            ID3Learning id3learning = new ID3Learning(tree);
-
-            // Translate our training data into integer symbols using our codebook:
-            //DataTable symbols = codebook.Apply(table);
-
-            int yk = 0;
-            int[][] inputs = new int[10][];//catHistograms.Count()][];
-
-            foreach(var item in catHistograms)
-            {
-                inputs[yk] = new int[10];//[item.Count()];
-                Array.Copy(item.ToArray(), inputs[yk], 10);//item.ToList().Count());
-                yk++;
-            }
-
-            int[] outputs = new int[10];
-
-            // Learn the training instances!
-            id3learning.Run(inputs, outputs);
-
-
-
-
-            //int[] query = codebook.Translate("Sunny", "Hot", "High", "Strong");
-
-            //int output = tree.Compute(query);
-
-            //string answer = codebook.Translate("PlayTennis", output);
-            // answer will be "No".
         }
 
-        private static List<BitmapFeature> Perform(string path, int amount)
+        private static DecisionTree GenerateDecisionTree(int amountOfDecisionVariables, int sections, int amountOfClassCount)
         {
-            Size avgSize = new Size(500, 500);
+            List<DecisionVariable> attributes = new List<DecisionVariable>();
+            for(int i = 0; i < amountOfDecisionVariables; i++)
+            {
+                attributes.Add(new DecisionVariable(i.ToString(), new AForge.IntRange(0, sections)));
+            }
+
+            DecisionTree tree = new DecisionTree(attributes.ToArray(), amountOfClassCount);
+            return tree;
+        }
+
+        private static List<BitmapFeature> Perform(string path, int amount, int from, int roundTo, int shredBy, out int shrededElementsPerBitmap)
+        {
+            Size avgSize = new Size(400, 350);
 
             //1. Load collection
-            var bmpCollection = BitmapFactory.BitmapFactory.LoadFromDirectory(path, amount).ToList();
+            var bmpCollection = BitmapFactory.BitmapFactory.LoadFromDirectory(path, amount, from).ToList();
 
             //2. Calculate average size of images from collection
-            avgSize = bmpCollection.AverageSize().RoundSquareTo(50);
+            //avgSize = bmpCollection.AverageSize().RoundSquareTo(roundTo);
 
             //3. Resize images
             bmpCollection = bmpCollection.ResizeImages(avgSize).ToList();
 
             //4. Shred images
-            var shreddedCollection = bmpCollection.ShredImages(50).ToList();
+            var shreddedCollection = bmpCollection.ShredImages(shredBy).ToList();
+
+            shrededElementsPerBitmap = shreddedCollection.First().Count();
 
             //5. Calculate features
             List<BitmapFeature> bmpFeatures = new List<BitmapFeature>();
